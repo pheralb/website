3f8a031ee16299a42ce6ceeff3602c031867a848
pheralb <pablohdez4340@gmail.com>
2022-11-26 16:03:43 +0000

    🚀 Initial commit.

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "src",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "api/index.js",
+  // publicPath: "/build/",
+};
