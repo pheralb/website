031b89f5b9ec08bd7717a17a7d914c9fafaa6232
pheralb <pablohdez4340@gmail.com>
2022-11-26 17:56:51 +0000

    ⚒️ Add custom fonts & colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", "sans-serif"],
+        mono: ["Cascadia", "monospace"],
+      },
+      colors: {
+        night: "#121212",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
