2ecbb5697edde16ec884675424cf00b396094b59
pheralb <pablohdez4340@gmail.com>
2022-11-27 00:23:44 +0000

    ⚒️ MDX config.

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,14 @@ module.exports = {
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "src",
+  mdx: async () => {
+    const [rehypePrismPlus] = await Promise.all([
+      import("rehype-prism-plus").then((module) => module.default),
+    ]);
+    return {
+      rehypePlugins: [rehypePrismPlus],
+    };
+  },
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
