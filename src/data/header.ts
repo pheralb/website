ce43b1389cb06ddab16fa81bc6568bb74a2026bf
pheralb <pablohdez4340@gmail.com>
2022-12-06 20:37:33 +0000

    ⚒️ Create header data.

---
+++ src/data/header.ts
@@ -0,0 +1,14 @@
+export const headerLinksData = [
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Usage",
+    href: "/usage",
+  },
+  {
+    title: "Apps",
+    href: "/apps",
+  },
+];
