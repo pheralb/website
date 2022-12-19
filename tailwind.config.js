61b3d1ce30a85e479db65499dc57731f236160a2
pheralb <pablohdez4340@gmail.com>
2022-12-19 13:59:09 +0000

    ⚒️ Add custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
         mono: ["var(--font-jetbrains-mono)", ...fontFamily.mono],
+      },
+      colors: {
+        light: "#FFFEFC",
+        midnight: "#121212",
       },
     },
   },
