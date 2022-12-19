7ff7d8c84ec3c2cc1884f26b193214f7769becc4
pheralb <pablohdez4340@gmail.com>
2022-12-19 13:41:06 +0000

    ⚒️ Add fonts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+// Global styles =>
+import "@/styles/globals.css";
+
+// Fonts =>
+import { Inter, JetBrains_Mono } from "@next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${inter.variable} ${jetBrainsMono.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
