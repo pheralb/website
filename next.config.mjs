d4fc8c1cc86a319f4043021bfaef9412c0b1b13c
pheralb <pablohdez4340@gmail.com>
2023-01-04 23:06:53 +0000

    ⚒️ Add redirects.

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,30 @@ const nextConfig = {
       "midu.dev",
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/tw",
+        destination: "https://twitter.com/pheralb_",
+        permanent: true,
+      },
+      {
+        source: "/tv",
+        destination: "https://www.twitch.tv/pheralb",
+        permanent: true,
+      },
+      {
+        source: "/gh",
+        destination: "https://github.com/pheralb",
+        permanent: true,
+      },
+      {
+        source: "/reapi",
+        destination: "https://reapi.pheralb.workers.dev/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withContentlayer(nextConfig);
