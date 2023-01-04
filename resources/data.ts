59bf74da1c4c2b41051fb7dccf7d56ec7e63ff49
pheralb <pablohdez4340@gmail.com>
2023-01-04 22:39:23 +0000

    ⚒️ Initial api commit.

---
+++ resources/data.ts
@@ -0,0 +1,56 @@
+export const resources = [
+  {
+    title: "midudev",
+    category: "people",
+    description:
+      "Programming Content Creator and Web Development Discloser on Twitch.",
+    url: "https://midu.dev/",
+    img: "/logos/midudev.ico",
+  },
+  {
+    title: "rauno.me",
+    category: "people",
+    description: "Creating software that makes people feel something.",
+    url: "https://rauno.me/",
+    img: "/logos/rauno.svg",
+  },
+  {
+    title: "ui.land",
+    category: "design",
+    description: "The Digital Library for Designers and Engineers",
+    url: "https://ui.land/",
+    img: "/logos/uiland.ico",
+  },
+  {
+    title: "Inter",
+    category: "fonts",
+    description:
+      "Inter is a free and open source font family designed by Rasmus Andersson.",
+    url: "https://rsms.me/inter/",
+    img: "/logos/interrsms.png",
+  },
+  {
+    title: "Fontshare",
+    category: "fonts",
+    description:
+      "Fontshare is a free fonts service from the Indian Type Foundry (ITF), making quality fonts accessible to all.",
+    url: "https://www.fontshare.com/",
+    img: "/logos/fontshare.png",
+  },
+  {
+    title: "Phosphor Icons",
+    category: "icons",
+    description:
+      "A flexible icon family for interfaces, diagrams, presentations — whatever, really.",
+    url: "https://phosphoricons.com/",
+    img: "/logos/phosphoricons.ico",
+  },
+  {
+    title: "Heroicons",
+    category: "icons",
+    description:
+      "A set of free MIT-licensed high-quality SVG icons for you to use in your web projects.",
+    url: "https://heroicons.com/",
+    img: "/logos/heroicons.png",
+  },
+];
