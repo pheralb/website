5923b08f93681147f85d4670fd8b153d27b82b5e
pheralb <pablohdez4340@gmail.com>
2023-01-05 00:29:11 +0000

    ⚒️ Add seo component.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 
 // Analytics =>
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 // Theme & global styles =>
 import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
-//
+
+// SEO =>
+import { DefaultSeo } from "next-seo";
+import nextSeoConfig from "next-seo.config";
 
 // Other =>
 import Header from "@/layout/header";
@@ -16,6 +19,7 @@ import Warning from "@/components/warning";
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <DefaultSeo {...nextSeoConfig} />
       <Warning />
       <Header />
       <Routing key={router.route}>
