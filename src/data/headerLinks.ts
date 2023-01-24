4516eb44f7864fcb3b0eeb0dc6c22b6a529b6025
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2023-01-24 09:52:55 +0000

    🛠 Add data links.

---
+++ src/data/headerLinks.ts
@@ -0,0 +1,10 @@
+export const headerLinks = [
+    {
+        title: "Blog",
+        url: "/blog",
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+    }
+];
---
+++ src/data/socialLinks.tsx
@@ -0,0 +1,19 @@
+// Icons =>
+import { GitHub, LinkedIn, Twitter } from "iconoir-react";
+
+const iconSize = 22;
+
+export const socialsLinks = [
+  {
+    url: "https://twitter.com/pheralb_",
+    icon: <Twitter width={iconSize} height={iconSize} name="Twitter" />,
+  },
+  {
+    url: "https://github.com/pheralb",
+    icon: <GitHub width={iconSize} height={iconSize} name="Twitter" />,
+  },
+  {
+    url: "https://www.linkedin.com/in/pheralb/",
+    icon: <LinkedIn width={iconSize} height={iconSize} name="Linkedin" />,
+  },
+];
