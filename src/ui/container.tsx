c4d69fc25f02a4efdff59c46b27c42af67ab1a19
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2023-01-24 11:41:45 +0000

    🛠 Update container.

--- src/ui/container.tsx
+++
@@ -1,9 +0,0 @@
-interface CustomContainerProps {
-  children: React.ReactNode;
-}
-
-const CustomContainer = (props: CustomContainerProps) => {
-  return <div className="max-w-6xl p-4 mx-auto md:p-0">{props.children}</div>;
-};
-
-export default CustomContainer;
