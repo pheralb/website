82a1f0c756cdb82860a11f7423fffb840d7215ec
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2023-01-27 09:42:40 +0000

    Fix socials component.

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,12 +1,12 @@
-import socialsLinks from "../data/socialsLinks";
+import { iSocials, socialsLinks } from "../data/socialLinks";
 
 const Socials = () => {
   return (
-    <div class="flex items-center justify-center md:pl-6 space-x-5 md:border-l mt-5 md:mt-0 border-neutral-700">
-      {socialsLinks.map((link) => (
+    <div className="flex items-center justify-center md:pl-6 space-x-5 md:border-l mt-5 md:mt-0 border-neutral-700">
+      {socialsLinks.map((link : iSocials) => (
         <a
           href={link.url}
-          class="text-gray-400 transition-all duration-150 hover:text-white"
+          className="text-gray-400 transition-all duration-150 hover:text-white"
           target="_blank"
         >
           {link.icon}
