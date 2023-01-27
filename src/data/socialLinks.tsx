4af23a4cb8285df5e648907e2b7091444ee530b8
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2023-01-27 13:58:28 +0000

    Add strokeWidth prop.

--- src/data/socialLinks.tsx
+++ src/data/socialLinks.tsx
@@ -1,7 +1,8 @@
-import { GitHub, LinkedIn, Twitter } from "iconoir-react";
 import type { ReactNode } from "react";
+import { GitHub, LinkedIn, Twitter, Heart } from "iconoir-react";
 
 const iconSize = 22;
+const stroke = 2;
 
 export interface iSocials {
   url: string;
@@ -11,14 +12,46 @@ export interface iSocials {
 export const socialsLinks = [
   {
     url: "https://twitter.com/pheralb_",
-    icon: <Twitter width={iconSize} height={iconSize} name="Twitter" />,
+    icon: (
+      <Twitter
+        width={iconSize}
+        height={iconSize}
+        strokeWidth={stroke}
+        name="Twitter"
+      />
+    ),
   },
   {
     url: "https://github.com/pheralb",
-    icon: <GitHub width={iconSize} height={iconSize} name="Twitter" />,
+    icon: (
+      <GitHub
+        width={iconSize}
+        height={iconSize}
+        strokeWidth={stroke}
+        name="Github"
+      />
+    ),
   },
   {
     url: "https://www.linkedin.com/in/pheralb/",
-    icon: <LinkedIn width={iconSize} height={iconSize} name="Linkedin" />,
+    icon: (
+      <LinkedIn
+        width={iconSize}
+        height={iconSize}
+        strokeWidth={stroke}
+        name="Linkedin"
+      />
+    ),
+  },
+  {
+    url: "https://github.com/sponsors/pheralb",
+    icon: (
+      <Heart
+        width={iconSize}
+        height={iconSize}
+        strokeWidth={stroke}
+        name="Github Sponsors"
+      />
+    ),
   },
 ];
