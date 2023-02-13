9d57094cd0a5e1172a201d6a99e56dc4b5187d2b
pheralb <pablohdez4340@gmail.com>
2023-02-13 10:55:38 +0000

    🛠️ Fix key.

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -5,8 +5,8 @@ const Socials = () => {
     <div className="flex items-center justify-center md:pl-6 space-x-5 md:border-l mt-5 md:mt-0 border-neutral-700">
       {socialsLinks.map((link : iSocials) => (
         <a
+          key={link.url}
           href={link.url}
-          
           target="_blank"
         >
           {link.icon}
